ca9dbb80e0d9af4706b06d9c37a8771476f55950
cocolilo <cocolilo148@gmail.com>
2019-11-25 00:45:40 -0300

    añadiendo makefile y ordenando los archivos

---
+++ tareasProgra/tarea2_Programacion/src/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h> //Para los números aleatorios
+#include <time.h> //Para los números aleatorios
+#include "toqueFama.h"
+#include "conecta4.h"
+
+int main(int argc, char **argv) {
+	
+	toqueFama();
+	return 0;
+}

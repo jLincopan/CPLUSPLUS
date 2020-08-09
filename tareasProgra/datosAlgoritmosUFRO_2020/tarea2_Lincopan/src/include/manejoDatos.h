753a2a19a7d1c2318c869102e90496298c125bf2
Javier <cocolilo148@gmail.com>
2020-08-09 07:53:35 -0400

    arreglando un poco la indentación

--- tareasProgra/datosAlgoritmosUFRO_2020/tarea2_Lincopan/src/include/manejoDatos.h
+++ tareasProgra/datosAlgoritmosUFRO_2020/tarea2_Lincopan/src/include/manejoDatos.h
@@ -3,54 +3,54 @@
     #define BUFFER_SIZE 40
     #include <stdlib.h>
 
-struct Cliente{
-	int id;
-	char nombre[15];
-	char apellido[15];
-	char rut[10];
-	long telefono;
-	char direccion[50];
-	int edad;
-	char profesion[30];
-	long deuda;
-	char fechaCobro[11];
-};
+	struct Cliente{
+		int id;
+		char nombre[15];
+		char apellido[15];
+		char rut[10];
+		long telefono;
+		char direccion[50];
+		int edad;
+		char profesion[30];
+		long deuda;
+		char fechaCobro[11];
+	};
 
-struct nodoCliente{
-	Cliente dato;
-	nodoCliente*sgte;
-};
+	struct nodoCliente{
+		Cliente dato;
+		nodoCliente*sgte;
+	};
 
-struct cabeceraCliente{
-	int largo;
-	nodoCliente* primero;
-};
+	struct cabeceraCliente{
+		int largo;
+		nodoCliente* primero;
+	};
 	
-struct Vendedor{
-	long cuenta;
-	char nombre[15];
-	char apellido[15];
-	char rut[10];
-	long telefono;
-	char direccion[50];
-	int edad;
-	char profesion[30];
-	long deuda;
-	cabeceraCliente* clientes;
-};
+	struct Vendedor{
+		long cuenta;
+		char nombre[15];
+		char apellido[15];
+		char rut[10];
+		long telefono;
+		char direccion[50];
+		int edad;
+		char profesion[30];
+		long deuda;
+		cabeceraCliente* clientes;
+	};
 	
-struct nodoVendedor{
-	Vendedor dato;
-	nodoVendedor* sgte;
-};
+	struct nodoVendedor{
+		Vendedor dato;
+		nodoVendedor* sgte;
+	};
 
-struct cabeceraVendedor{
-	int largo;
-	nodoVendedor* primero; 
-};  
+	struct cabeceraVendedor{
+		int largo;
+		nodoVendedor* primero; 
+	};  
 
 
-void registrarVendedor(Vendedor &buffer);
-void leerLinea_texto(size_t largo, char buffer[]);
-void mostrarDatos_vendedor(Vendedor vendedor);
+	void registrarVendedor(Vendedor &buffer);
+	void leerLinea_texto(size_t largo, char buffer[]);
+	void mostrarDatos_vendedor(Vendedor vendedor);
 #endif
